65525fc7068f0275d49605e9e34eb8242043b8ac
Johannes <johannes.programming@gmail.com>
2026-02-20 02:23:48 +0100

    Version 1.0.4

--- src/iterprod/tests/__init__.py
+++ src/iterprod/tests/__init__.py
@@ -6,7 +6,7 @@ __all__ = ["test"]
 def test() -> unittest.TextTestRunner:
     "This function runs all the tests."
     loader: unittest.TestLoader
-    tests: unittest.TestSuite
+    suite: unittest.TestSuite
     loader = unittest.TestLoader()
-    tests = loader.discover(start_dir="iterprod.tests")
-    return unittest.TextTestRunner().run(tests)
+    suite = loader.discover(start_dir="iterprod.tests")
+    return unittest.TextTestRunner().run(suite)
