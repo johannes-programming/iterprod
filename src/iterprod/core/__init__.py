8360009cac0f4424df598a30188c8b4da1a7a29c
Johannes <johannes.programming@gmail.com>
2026-06-20 00:50:22 +0200

    Version 1.0.5

--- src/iterprod/core/__init__.py
+++ src/iterprod/core/__init__.py
@@ -1,18 +1,25 @@
 import operator
-from typing import *
+from collections.abc import Generator, Iterable
+from typing import Any, SupportsIndex
 
 __all__ = ["iterprod"]
 
 
-def iterprod(*iterables: Iterable, repeat: SupportsIndex = 1) -> Generator:
-    if repeat < 0:
+def iterprod(
+    *iterables: Iterable[Any],
+    repeat: SupportsIndex = 1,
+) -> Generator[tuple[Any, ...], None, None]:
+    indeces: list[int]
+    lengths: tuple[int, ...]
+    pools: list[tuple[Any, ...]]
+    repeat_: int
+    repeat_ = operator.index(repeat)
+    if repeat_ < 0:
         raise ValueError("repeat argument cannot be negative")
-    indeces: list
-    lengths: tuple
-    pools: Iterable
-    pools = map(tuple, iterables)
-    pools = list(pools)
-    pools *= repeat
+    pools = list(map(tuple, iterables))
+    if () in pools:
+        return
+    pools *= repeat_
     indeces = [0] * len(pools)
     lengths = tuple(map(len, pools))
     while True:
@@ -23,7 +30,7 @@ def iterprod(*iterables: Iterable, repeat: SupportsIndex = 1) -> Generator:
             return
 
 
-def incr(indeces: list, lengths: tuple) -> None:
+def incr(indeces: list[int], lengths: tuple[int, ...]) -> None:
     j: int
     indeces[-1] += 1
     j = -1
