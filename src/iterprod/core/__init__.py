4945b67d0eb3e962c75854f0b08ab8e71fb72768
Johannes <johannes.programming@gmail.com>
2026-06-20 11:44:58 +0200

    Version 1.1.10

--- src/iterprod/core/__init__.py
+++ src/iterprod/core/__init__.py
@@ -1,42 +1,41 @@
 import operator
-from collections.abc import Generator, Iterable
-from typing import Any, SupportsIndex
+from collections.abc import Generator, Iterable, Sequence
+from typing import Any, SupportsIndex, TypeVar
 
 __all__ = ["iterprod"]
 
+Item = TypeVar("Item")
+
+
+def get_total_and_infos(
+    *iterables: Iterable[Item],
+    repeat: SupportsIndex = 1,
+) -> tuple[int, Sequence[tuple[int, tuple[Item, ...]]]]:
+    div: int
+    infos: list[Any]
+    pools: list[Any]
+    repeat_: int
+    pools = list(map(tuple, iterables))
+    repeat_ = operator.index(repeat)
+    pools *= abs(repeat_)
+    if repeat_ < 0:
+        pools.reverse()
+    div = 1
+    infos = list()
+    for pool in reversed(pools):
+        infos.append((div, pool))
+        div *= len(pool)
+    return div, tuple(reversed(infos))
+
 
 def iterprod(
-    *iterables: Iterable[Any],
+    *iterables: Iterable[Item],
     repeat: SupportsIndex = 1,
-) -> Generator[tuple[Any, ...], None, None]:
-    indeces: list[int]
-    lengths: tuple[int, ...]
-    pools: list[tuple[Any, ...]]
-    repeat_: int
-    repeat_ = operator.index(repeat)
-    if repeat_ < 0:
-        raise ValueError("repeat argument cannot be negative")
-    pools = list(map(tuple, iterables))
-    if () in pools:
-        return
-    pools *= repeat_
-    indeces = [0] * len(pools)
-    lengths = tuple(map(len, pools))
-    while True:
-        yield tuple(map(operator.getitem, pools, indeces))
-        try:
-            incr(indeces, lengths)
-        except IndexError:
-            return
-
-
-def incr(indeces: list[int], lengths: tuple[int, ...]) -> None:
-    j: int
-    indeces[-1] += 1
-    j = -1
-    while True:
-        if indeces[j] < lengths[j]:
-            return
-        indeces[j] = 0
-        indeces[j - 1] += 1
-        j -= 1
+) -> Generator[tuple[Item, ...], None, None]:
+    "This generator iterates the Cartesian product of the given iterables."
+    count: int
+    infos: Sequence[tuple[int, tuple[Item, ...]]]
+    total: int
+    total, infos = get_total_and_infos(*iterables, repeat=repeat)
+    for count in range(total):
+        yield tuple(pool[count // div % len(pool)] for div, pool in infos)
